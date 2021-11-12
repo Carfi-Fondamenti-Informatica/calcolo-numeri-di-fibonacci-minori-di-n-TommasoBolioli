2e5023f339865d75594ee2b047d8e4e84a61c444
TommasoBolioli <91489369+TommasoBolioli@users.noreply.github.com>
2021-11-12 15:25:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,15 @@ int main() {
     int n = 0, i = 0, primo_numero = 1, secondo_numero = 0, numero_successivo = 0;
     cin >> n;
 
-/*I comandi per la serie di fibonacci sono gli stessi  dell'esercitazione 4, ma per rispettare
- * la consegna è stata aggiunta la struttura condizionale if che blocca la stampa a video se il risultato
- * della serie è maggiore del numero n in ingresso*/
-
 if(n==1){
     cout << primo_numero << endl;
     cout << primo_numero << endl;
 
 }else{
-            for (i = 0; i < n; i++) {
-                numero_successivo = primo_numero + secondo_numero;
-                primo_numero = secondo_numero;
-            secondo_numero = numero_successivo;
+    for (i = 0; i < n; i++) {
+        numero_successivo = primo_numero + secondo_numero;
+        primo_numero = secondo_numero;
+        secondo_numero = numero_successivo;
             if(numero_successivo<= n){
                 cout << numero_successivo << endl;}
     }}}
